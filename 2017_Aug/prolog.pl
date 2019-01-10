645b7f0054370d81a68e4b4cf59a4c6465494278
Rian Gallagher <rianogallagher@gmail.com>
2019-01-10 19:05:15 +0000

    add prolog answer for Aug 2017

--- 2017_Aug/prolog.pl
+++ 2017_Aug/prolog.pl
@@ -1,4 +1,11 @@
-thrice(A,X) :- containsInOrder(A,X,0).
+thrice(Item, List) :-
+  thriceHelper(Item, List, 0).
 
-containsInOrder(A, [X|Xs], N) :- ((A == X) ->  N1 is N+1; N1 is N+0), containsInOrder(A, Xs, N1).
-containsInOrder(_, [], 3).
+thriceHelper(Item, [], Count):-
+  Count = 3, !.
+  
+thriceHelper(Item, [H|T], Count) :-
+  Item = H, !,
+  NewCount is Count + 1,
+  thriceHelper(Item, T, NewCount)
+  ; thriceHelper(Item, T, Count).
