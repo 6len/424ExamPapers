918c679eea308669424d704b51efe45658520ed0
Glen Cloughley <glen.cloughley.2016@mumail.ie>
2019-01-14 10:43:07 +0000

    neat transpose function

--- 2016_Aug/prolog.pl
+++ 2016_Aug/prolog.pl
@@ -1,11 +1,6 @@
-transp([X|XS], [Y|YS]) :- transp(X, XS, Y, YS, YS).
-transp([X|XR], XS, [Y|YR], [YT|YS], YA) :- write(X), write(YT), transp(XR, XS, YS, YS, YA).
-transp(X, XS, Y, YS, YA) :- transp(XS, YS).
-transp([], [], [], [], []).
-
-tr([X|XS], [Y|YS]) :- tr(X, Y, XS, YS).
-tr([X|XR], [Y|YR], XS, [YT|YS]) :- write(X), write(Y), tr(XR, YT, XS, YS).
-tr([X|_], [Y|YR], XS, YS):- ((X == Y) -> write(X)).
-tr([], [], [], []).
-
-%% doesn't work at the moment
+tr(X, Y) :- tr(X, Y, 0).
+tr([X|XS], Y, N) :- N1 is N+1, ((trCheck(X, Y, N) -> tr(XS, Y, N1))).
+tr([], _, _).
+    
+trCheck([X|XS], [Y|YS] , N) :- ((nth0(N, Y, X)) -> trCheck(XS, YS, N)).
+trCheck([], [], _).
